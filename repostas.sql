f23350cd48a009862f784c7d250c5b2152f981cb
PEDRO HENRIQUE CONZATTI BRENZINK <pedro.brenzink@catolicasc.edu.br>
2026-05-05 11:09:20 -0300

    att

--- repostas.sql
+++ repostas.sql
@@ -13,23 +13,36 @@ INNER JOIN passageiro ON fk_passageiro = pk_passageiro
 WHERE status = 'concluida';
 
 --Questão 4
-SELECT pk_categoria, preco_base_km, motorista.nome
-FROM categoria_veiculo
-INNER JOIN motorista ON pk_categoria = pk_motorista;
+SELECT categoria_veiculo.nome, preco_base_km, motorista.nome
+FROM motorista
+INNER JOIN veiculo ON fk_motorista = pk_motorista
+INNER JOIN categoria_veiculo ON pk_motorista = fk_motorista;
 
 --Questão 5
-SELECT motorista.nome AS motorista, passageiro.nome AS passageiro, valor_total, pk_categoria 
+SELECT motorista.nome AS motorista, passageiro.nome AS passageiro, valor_total, categoria_veiculo.nome 
 FROM motorista
 INNER JOIN passageiro ON pk_passageiro = pk_motorista
 INNER JOIN viagem ON pk_passageiro = fk_passageiro
-INNER JOIN categoria_veiculo ON pk_categoria = pk_motorista;
+INNER JOIN categoria_veiculo ON pk_categoria = fk_categoria;
 
 --Questão 6
 SELECT motorista.nome AS motorista, passageiro.nome AS passageiro, valor_total, descricao
 FROM motorista
-INNER JOIN passageiro ON pk_passageiro = pk_motorista
+INNER JOIN passageiro ON pk_motorista = fk_motorista
 INNER JOIN viagem ON pk_passageiro = fk_passageiro
 INNER JOIN meio_pagamento ON pk_meio_pagamento = fk_meio_pagamento;
+
+--Questão 7
+
+SELECT motorista.nome AS motorista, passageiro.nome AS passageiro, nota_motorista_ao_passageiro, nota_passageiro_ao_motorista
+FROM motorista
+INNER JOIN 
+
+--Questão 8
+
+--Questão 9
+
+--Questão 10
 
 --Questão 15 
 SELECT pk_pagamento, descricao FROM pagamento
